688bfba845a93d8988f2cb64bd6b505e81c2ddd0
Jason Kim <actcrazy@gmail.com>
2019-09-25 17:40:31 +0900

    4장 Either

---
+++ src/main/scala/chapter04/Either.scala
@@ -0,0 +1,50 @@
+package chapter04
+
+import scala.{Option => _, Either => _, Left => _, Right => _, _} // hide std library `Option` and `Either`, since we are writing our own in this chapter
+
+sealed trait Either[+E,+A] {
+  def map[B](f: A => B): Either[E, B] = this match {
+    case Right(a) => Right(f(a))
+    case Left(e) => Left(e)
+  }
+
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+    case Right(a) => f(a)
+    case Left(e) => Left(e)
+  }
+
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+    case Left(_) => b
+    case r => r
+  }
+
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+    for {
+      x <- this
+      y <- b
+    } yield f(x, y)
+}
+case class Left[+E](get: E) extends Either[E,Nothing]
+case class Right[+A](get: A) extends Either[Nothing,A]
+
+object Either {
+  def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] =
+    es.foldRight[Either[E, List[B]]](Right(Nil))((h, t) => f(h).map2(t)(_ :: _))
+
+  def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] =
+    traverse(es)(x => x)
+
+  def mean(xs: IndexedSeq[Double]): Either[String, Double] = 
+    if (xs.isEmpty) 
+      Left("mean of empty list!")
+    else 
+      Right(xs.sum / xs.length)
+
+  def safeDiv(x: Int, y: Int): Either[Exception, Int] = 
+    try Right(x / y)
+    catch { case e: Exception => Left(e) }
+
+  def Try[A](a: => A): Either[Exception, A] =
+    try Right(a)
+    catch { case e: Exception => Left(e) }
+}
