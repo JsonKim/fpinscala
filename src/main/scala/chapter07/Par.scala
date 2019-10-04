efda7debfd752550248b700becec77ed0bfe985d
Jason Kim <actcrazy@gmail.com>
2019-10-04 10:37:40 +0900

    7.1.1 Par 기본 구현

---
+++ src/main/scala/chapter07/Par.scala
@@ -0,0 +1,30 @@
+package chapter07
+
+object Par {
+  case class Par[A](value: A)
+
+  def unit[A](a: => A): Par[A] = Par(a)
+
+  def get[A](a: Par[A]): A = a.value
+}
+
+object Examples {
+  import Par._
+  def sum(ints: IndexedSeq[Int]): Int = // `IndexedSeq` is a superclass of random-access sequences like `Vector` in the standard library. Unlike lists, these sequences provide an efficient `splitAt` method for dividing them into two parts at a particular index.
+    if (ints.size <= 1)
+      ints.headOption getOrElse 0 // `headOption` is a method defined on all collections in Scala. We saw this function in chapter 3.
+    else { 
+      val (l,r) = ints.splitAt(ints.length/2) // Divide the sequence in half using the `splitAt` function.
+      sum(l) + sum(r) // Recursively sum both halves and add the results together.
+    }
+
+  def sum_1(ints: IndexedSeq[Int]): Int =
+    if (ints.size <= 1)
+      ints.headOption getOrElse 0
+    else {
+      val (l, r) = ints.splitAt(ints.length / 2)
+      val sumL: Par[Int] = unit(sum(l))
+      val sumR: Par[Int] = unit(sum(r))
+      get(sumL) + get(sumR)
+    }
+}
