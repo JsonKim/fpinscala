9eef295187ff74b51992cf96976f3f7483007455
Jason Kim <actcrazy@gmail.com>
2019-10-04 22:43:01 +0900

    7.4 비차단(non-blocking) Par 구현

---
+++ src/main/scala/chapter07/Nonblocking.scala
@@ -0,0 +1,88 @@
+package chapter07
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.atomic.AtomicReference
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.Callable
+
+object Nonblocking {
+  sealed trait Future[+A] {
+    private[chapter07] def apply(cb: A => Unit): Unit
+  }
+  type Par[+A] = ExecutorService => Future[A]
+
+  object Par {
+    def run[A](es: ExecutorService)(p: Par[A]): A = {
+      val ref = new AtomicReference[A]
+      val latch = new CountDownLatch(1)
+      p(es) { a => ref.set(a); latch.countDown }
+      latch.await
+      ref.get
+    }
+
+    def unit[A](a: A): Par[A] =
+      es => new Future[A] {
+        def apply(cb: A => Unit): Unit =
+          cb(a)
+      }
+
+    def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
+
+    def eval(es: ExecutorService)(r: => Unit): Unit =
+      es.submit(new Callable[Unit] { def call = r })
+
+    def fork[A](a: => Par[A]): Par[A] =
+      es => new Future[A] {
+        def apply(cb: A => Unit): Unit =
+          eval(es)(a(es)(cb))
+      }
+
+    def asyncF[A,B](f: A => B): A => Par[B] =
+      a => lazyUnit(f(a))
+
+    def map2[A,B,C](pa: Par[A], pb: Par[B])(f: (A,B) => C): Par[C] =
+      es => new Future[C] {
+        def apply(cb: C => Unit): Unit = {
+          var ar: Option[A] = None
+          var br: Option[B] = None
+
+          val combiner = Actor[Either[A,B]](es) {
+            case Left(a) => br match {
+              case None => ar = Some(a)
+              case Some(b) => eval(es)(cb(f(a, b)))
+            }
+            case Right(b) => ar match {
+              case None => br = Some(b)
+              case Some(a) => eval(es)(cb(f(a, b)))
+            }
+          }
+
+          pa(es)(a => combiner ! Left(a))
+          pb(es)(b => combiner ! Right(b))
+        }
+      }
+
+    // specialized version of `map`
+    def map[A,B](p: Par[A])(f: A => B): Par[B] =
+      es => new Future[B] {
+        def apply(cb: B => Unit): Unit =
+          p(es)(a => eval(es) { cb(f(a)) })
+      }
+
+    def sequenceBalanced[A](as: IndexedSeq[Par[A]]): Par[IndexedSeq[A]] = fork {
+      if (as.isEmpty) unit(Vector())
+      else if (as.length == 1) map(as.head)(a => Vector(a))
+      else {
+        val (l,r) = as.splitAt(as.length/2)
+        map2(sequenceBalanced(l), sequenceBalanced(r))(_ ++ _)
+      }
+    }
+
+    def sequence[A](as: List[Par[A]]): Par[List[A]] =
+      map(sequenceBalanced(as.toIndexedSeq))(_.toList)
+
+    def parMap[A,B](ps: List[A])(f: A => B): Par[List[B]] = fork {
+      val fbs: List[Par[B]] = ps.map(asyncF(f))
+      sequence(fbs)
+    }
+  }
+}
