47cb18f6349e76b1201497e1e4cb9189334daebe
Jason Kim <actcrazy@gmail.com>
2019-10-04 11:44:42 +0900

    7.2 주석

--- src/main/scala/chapter07/Par.scala
+++ src/main/scala/chapter07/Par.scala
@@ -3,13 +3,21 @@ package chapter07
 object Par {
   case class Par[A](value: A)
 
+  // 즉시 평가되어서 결과 a를 산출하는 계산을 생성.
+  // 상수 값을 병렬 계산으로 승격
   def unit[A](a: A): Par[A] = Par(a)
+  // run이 동시적으로 평가할 표현식 a를 감싼다
+  // 평가 되지 않은 인수를 Par로 감싸고 병렬 평가 대상으로 표시
   def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
 
+  // 주어진 Par를 fork의 요청에 따라 병렬 계산을 수행하고 그 결과값을 추출함으로써 완전히 평가
+  // 계산의 실제 실행
   def run[A](a: Par[A]): A = a.value
 
+  // 두 병렬 계산의 결과들을 이항 함수로 조합
   def map2[A,B,C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = unit(f(run(a), run(b)))
 
+  // 이후에 run이 동시적으로 평가할 계산임을 표시. run에 강제 되어야 실제로 평가
   def fork[A](a: => Par[A]): Par[A] = a
 }
 
