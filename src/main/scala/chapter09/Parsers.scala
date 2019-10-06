48f4dea52fbde5dd479224c909bc618b7d6f2809
Jason Kim <actcrazy@gmail.com>
2019-10-06 22:52:53 +0900

    formatting

--- src/main/scala/chapter09/Parsers.scala
+++ src/main/scala/chapter09/Parsers.scala
@@ -6,25 +6,38 @@ import chapter08.Prop._
 trait Parsers[ParseError, Parser[+_]] { self =>
 
   def run[A](p: Parser[A])(input: String): Either[ParseError,A]
+
   def char(c: Char): Parser[Char] =
     string(c.toString) map (_.charAt(0))
+
   def or[A](p1: Parser[A], p2: Parser[A]): Parser[A]
+
   implicit def string(s: String): Parser[String]
+
   implicit def operators[A](p: Parser[A]) = ParserOps[A](p)
+
   implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]):
     ParserOps[String] = ParserOps(f(a))
+
   def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]] =
     if (n < 1) succeed(List())
     else map2(p, listOfN(n-1, p))(_ :: _)
+
   def many[A](p: Parser[A]): Parser[List[A]] =
     map2(p, many(p))(_ :: _) or succeed(List())
+
   def many1[A](p: Parser[A]): Parser[List[A]] =
     map2(p, many(p))(_ :: _)
+
   def map[A,B](p: Parser[A])(f: A => B): Parser[B]
+
   def succeed[A](a: A): Parser[A] =
     string("") map (_ => a)
+
   def slice[A](p: Parser[A]): Parser[String]
+
   def product[A,B](p1: Parser[A], p2: Parser[B]): Parser[(A,B)]
+  
   def map2[A,B,C](p1: Parser[A], p2: Parser[B])(f: (A, B) => C): Parser[C] =
     product(p1, p2) map { case (a, b) => f(a, b) }
 
