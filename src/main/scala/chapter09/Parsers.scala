6e2a495aaf31f1e972c5de7bda21259bdf12c2e1
Jason Kim <actcrazy@gmail.com>
2019-10-06 22:24:02 +0900

    9.2 many, map 추가

--- src/main/scala/chapter09/Parsers.scala
+++ src/main/scala/chapter09/Parsers.scala
@@ -10,9 +10,13 @@ trait Parsers[ParseError, Parser[+_]] { self =>
   implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]):
     ParserOps[String] = ParserOps(f(a))
   def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]]
+  def many[A](p: Parser[A]): Parser[List[A]]
+  def map[A,B](p: Parser[A])(f: A => B): Parser[B]
 
   case class ParserOps[A](p: Parser[A]) {
     def |[B>:A](p2: Parser[B]): Parser[B] = self.or(p, p2)
     def or[B>:A](p2: Parser[B]): Parser[B] = self.or(p, p2)
+    def many: Parser[List[A]] = self.many(p)
+    def map[B](f: A => B): Parser[B] = self.map(p)(f)
   }
 }
