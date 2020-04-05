971985cbcc2e44b1a36f8228dec3fb913ba716bd
Jason Kim <actcrazy@gmail.com>
2020-04-05 13:18:42 +0900

    Prop.forAll에 잘못된 zip 구현 수정

--- src/main/scala/chapter08/Gen.scala
+++ src/main/scala/chapter08/Gen.scala
@@ -75,7 +75,7 @@ object Prop {
     Stream.unfold(rng)(rng => Some(g.sample.run(rng)))
 
   def forAll[A](as: Gen[A])(f: A => Boolean): Prop = Prop {
-    (n,rng) => randomStream(as)(rng).zip(Stream.from(0)).take(n).map {
+    (n,rng) => Stream.zip(randomStream(as)(rng), (Stream.from(0))).take(n).map {
       case (a, i) => try {
         if (f(a)) Passed else Falsified(a.toString, i)
       } catch { case e: Exception => Falsified(buildMsg(a, e), i) }
