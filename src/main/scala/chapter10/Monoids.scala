3185132af859df68a1f5e58add9057226d54ba25
Jason Kim <actcrazy@gmail.com>
2020-04-15 15:03:49 +0900

    연습문제 10.11

--- src/main/scala/chapter10/Monoids.scala
+++ src/main/scala/chapter10/Monoids.scala
@@ -152,7 +152,20 @@ object Monoid {
     def zero = Stub("")
   }
 
-  def count(s: String): Int = ???
+  def count(s: String): Int = {
+    def wc(c: Char): WC =
+      if (c.isWhitespace)
+        Part("", 0, "")
+      else
+        Stub(c.toString)
+
+    def unstub(s: String) = s.length min 1
+
+    foldMapV(s.toIndexedSeq, wcMonoid)(wc) match {
+      case Stub(s) => unstub(s)
+      case Part(l, w, r) => unstub(l) + w + unstub(r)
+    }
+  }
 
   def productMonoid[A,B](A: Monoid[A], B: Monoid[B]): Monoid[(A, B)] =
     ???
