be920cdb1ed6bda1a3be8fff34c26d2cbf865125
Jason Kim <actcrazy@gmail.com>
2020-04-18 11:05:44 +0900

    연습문제 11.17

--- src/main/scala/fpinscala/monads/Monad.scala
+++ src/main/scala/fpinscala/monads/Monad.scala
@@ -144,14 +144,18 @@ object Monad {
       ma flatMap f
   } 
 
-  val idMonad: Monad[Id] = ???
+  val idMonad: Monad[Id] = new Monad[Id] {
+    def unit[A](a: => A): Id[A] = Id(a)
+    def flatMap[A, B](ma: Id[A])(f: A => Id[B]): Id[B] =
+      ma flatMap f
+  }
 
   def readerMonad[R] = ???
 }
 
 case class Id[A](value: A) {
-  def map[B](f: A => B): Id[B] = ???
-  def flatMap[B](f: A => Id[B]): Id[B] = ???
+  def map[B](f: A => B): Id[B] = Id(f(a))
+  def flatMap[B](f: A => Id[B]): Id[B] = f(a)
 }
 
 object Reader {
